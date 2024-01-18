30047763a7e7710019dcc0d56176312c5389282e
ATK23-13\guru <alisa.kuzmina@kiteenkansanopisto.fi>
2024-01-18 10:41:06 +0200

    Tag tests

--- first-react-app/src/App.js
+++ first-react-app/src/App.js
@@ -1,3 +1,19 @@
 export function App() {
-  return <div>Challenge2024</div>;
+  return (
+    <div>
+      <div>
+        Challenge2024!!
+        Multiline
+      </div>
+      <h1>Challenge2024!!</h1>
+      <h2>Challenge2024!!</h2>
+      <b>Challenge2024!!</b><br />
+      <i>Challenge2024!!</i><hr />
+      <u>Challenge2024!!</u><br />
+      <p>
+        Challenge2024!!
+        Multiline
+      </p>
+    </div>
+  );
 }
