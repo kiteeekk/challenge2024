be7fdda4372cb7d8cf093443ed307b996f4b21f6
ATK23-13\guru <alisa.kuzmina@kiteenkansanopisto.fi>
2024-01-18 13:50:57 +0200

    Added cars listing

--- first-react-app/src/App.js
+++ first-react-app/src/App.js
@@ -1,9 +1,30 @@
+function selectColor(car) {
+  switch (car.make) {
+    case "Honda":
+      return "silver"
+    case "Ford":
+      return "blue"
+    case "Volkswagen":
+      return "green"
+    default:
+      return "red"
+  }
+}
+
 export function App() {
-  const fruits = ["Apple", "Orange", "Banana"]
+  const cars = [
+    { make: "Volvo", model: "V70", year: 2012 },
+    { make: "Honda", model: "Accord", year: 2000 },
+    { make: "Ford", model: "Fiesta", year: 2020 },
+    { make: "Volkswagen", model: "Golf", year: 2024 }
+  ]
+  const filteredCars = cars.filter(car => car.year >= 2010)
   return (
     <div>
       <ol>
-        {fruits.map(fruit => <li>{fruit}</li>)}
+        {filteredCars.map(car =>
+          <li style={{ color: selectColor(car) }}>{car.make} - {car.model} ({car.year})</li>
+        )}
       </ol>
     </div>
   );
