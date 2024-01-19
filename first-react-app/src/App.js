0ff9a73aa861fdc1e2176cbffc4cf8fd1778d4a9
ATK23-13\guru <alisa.kuzmina@kiteenkansanopisto.fi>
2024-01-19 11:02:09 +0200

    More filters

--- first-react-app/src/App.js
+++ first-react-app/src/App.js
@@ -20,20 +20,33 @@ export function App() {
     { make: "Ford", model: "Fiesta", year: 2020 },
     { make: "Volkswagen", model: "Golf", year: 2024 }
   ]
-  const [inputText, setInputText] = useState('')
-  console.log({ inputText })
-  const filteredCars = cars.filter(car =>
-    car.make.toLowerCase().includes(inputText.toLowerCase()) || // &&
-    car.model.toLowerCase().includes(inputText.toLowerCase())
-  )
+  const [inputText, setInputText] = useState("")
+  const [inputYear, setInputYear] = useState("")
+  const [inputSelect, setInputSelect] = useState("")
+  console.log({ inputText, inputYear, inputSelect })
+
+  const filteredCars =
+    cars
+      .filter(car =>
+        car.make.toLowerCase().includes(inputText.toLowerCase()) || // &&
+        car.model.toLowerCase().includes(inputText.toLowerCase())
+      )
+      .filter(car => car.year >= inputYear)
+      .sort((car1, car2) => car1.make.localeCompare(car2.make))
   return (
     <div>
       <input
-        type={"text"}
-        onChange={event => {
+        type={"text"} placeholder='Search' onChange={event => {
           setInputText(event.target.value)
         }}
       />
+      <input type={"number"} placeholder='Year' onChange={event => {
+        setInputYear(event.target.value)
+      }} />
+      <select onChange={event => setInputSelect(event.target.value)}>
+        <option>Make</option>
+        <option>Year</option>
+      </select>
       <ol>
         {filteredCars.map(car =>
           <li style={{ color: selectColor(car) }}>{car.make} - {car.model} ({car.year})</li>
