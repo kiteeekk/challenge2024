418380aebb366e4be1bab9a7b8c230b965ea25fc
ATK23-13\guru <alisa.kuzmina@kiteenkansanopisto.fi>
2024-01-25 12:48:39 +0200

    Allow adding and removing things

--- express-backend/server.js
+++ express-backend/server.js
@@ -8,9 +8,11 @@ app.use(bodyParser.json())
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
     next();
 });
 
+let nextCarIndex = 2;
 const db = [
     {
         "id": "1",
@@ -31,8 +33,16 @@ app.get('/cars/:id', function (req, res) {
 })
 
 app.post('/cars', function (req, res) {
-    console.log(req.body)
-    res.send('Hello')
+    const newCar = req.body;
+    newCar.id = `${nextCarIndex++}`
+    db.push(newCar)
+    res.send(newCar)
+})
+
+app.delete('/cars/:id', function (req, res) {
+    const index = db.findIndex(car => car.id === req.params.id);
+    db.splice(index, 1)
+    res.send()
 })
 
 app.listen(8000)
